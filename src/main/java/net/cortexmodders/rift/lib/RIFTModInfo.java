c43b133217bd8d537b2464730fd4b3b0f0889435
Jacob Rhoda <jake@jadarstudios.com>
2013-12-14 10:56:34 -0600

    ids and strings

---
+++ src/main/java/net/cortexmodders/rift/lib/RIFTModInfo.java
@@ -0,0 +1,12 @@
+package net.cortexmodders.rift.lib;
+
+public class RIFTModInfo
+{
+
+    public static final String MOD_ID = "rift";
+    public static final String MOD_NAME = "Rupture Invoked Field Teleporter";
+    public static final String MOD_VERSION = "${version}";
+    
+    public static final String CLIENT_PROXY = "net.cortexmodders.rift.proxy.ClientProxy";
+    public static final String COMMON_PROXY = "net.cortexmodders.rift.proxy.CommonProxy";
+}
