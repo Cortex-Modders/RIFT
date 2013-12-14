26d3ca71f1d2b60df8568f74a463f6e454270f85
Jacob Rhoda <jake@jadarstudios.com>
2013-12-13 20:56:55 -0600

    basic mod

--- src/main/java/net/cortexmodders/rift/RuptureInvokedFieldTransporter.java
+++ src/main/java/net/cortexmodders/rift/RuptureInvokedFieldTransporter.java
@@ -1,6 +1,37 @@
 package net.cortexmodders.rift;
 
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "RIFT", name = "Rupture Invoked Field Transporter", version = "0.0.1", useMetadata = true)
 public class RuptureInvokedFieldTransporter
 {
 
+    @Instance
+    public static RuptureInvokedFieldTransporter instance;
+    
+    
+    
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        
+    }
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        
+    }
+    
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+        
+    }
+    
 }
